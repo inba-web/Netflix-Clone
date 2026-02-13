358b9e51d96442acf33ae579ac1ffdc66b7ac928
Inba <inbavarunans@gmail.com>
2026-02-13 23:27:46 +0530

    implemented adsterra for monetize

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,44 +1,75 @@
-import React, { useState } from 'react';
-import './Login.css';
-import logo from '../../assets/logo.png'
-import { useNavigate } from 'react-router-dom';
-import { login, signup } from '../../firebase';
-import netflix_spinner from '../../assets/netflix_spinner.gif';
+import React, { useState } from "react";
+import "./Login.css";
+import logo from "../../assets/logo.png";
+import { useNavigate } from "react-router-dom";
+import { login, signup } from "../../firebase";
+import netflix_spinner from "../../assets/netflix_spinner.gif";
 
-const Login = () => {  
-
+const Login = () => {
   const [signState, setSignState] = useState("Sign In");
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);  
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   const user_auth = async (event) => {
-    event.preventDefault(); 
+    event.preventDefault();
     setLoading(true);
     if (signState === "Sign In") {
       await login(email, password);
     } else {
       await signup(name, email, password);
     }
-    setLoading(false);  
-  }
+    setLoading(false);
+  };
 
-  return (
-    loading?<div className="login-spinner"> 
-      <img src={netflix_spinner} alt=""/>
-    </div>:
-
-    <div className='login'>
-      <img src={logo} className='login-logo' onClick={() => navigate("/")} alt="" />
+  return loading ? (
+    <div className="login-spinner">
+      <img src={netflix_spinner} alt="" />
+    </div>
+  ) : (
+    <div className="login">
+      <img
+        src={logo}
+        className="login-logo"
+        onClick={() => navigate("/")}
+        alt=""
+      />
       <div className="login-form">
         <h1>{signState}</h1>
         <form>
-          {signState === "Sign Up" ? <input value={name} onChange={(e) => { setName(e.target.value) }} type="text" placeholder='Enter Your Name' /> : <></>}
-          <input value={email} onChange={(e) => { setEmail(e.target.value) }} type="text" placeholder='Enter Your Email' />
-          <input value={password} onChange={(e) => { setPassword(e.target.value) }} type="password" placeholder='Enter Your Password' />
-          <button onClick={user_auth} type='submit'>{signState}</button>
+          {signState === "Sign Up" ? (
+            <input
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              type="text"
+              placeholder="Enter Your Name"
+            />
+          ) : (
+            <></>
+          )}
+          <input
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            type="text"
+            placeholder="Enter Your Email"
+          />
+          <input
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="password"
+            placeholder="Enter Your Password"
+          />
+          <button onClick={user_auth} type="submit">
+            {signState}
+          </button>
           <div className="form-help">
             <div className="remember">
               <input type="checkbox" />
@@ -48,12 +79,21 @@ const Login = () => {
           </div>
         </form>
         <div className="form-switch">
-          {signState === "Sign In" ? <p>New to Netflix? <span onClick={() => setSignState("Sign Up")}>Sign Up Now</span></p>
-            : <p>Already have account? <span onClick={() => setSignState("Sign In")}>Sign In Now</span></p>}
+          {signState === "Sign In" ? (
+            <p>
+              New to Netflix?{" "}
+              <span onClick={() => setSignState("Sign Up")}>Sign Up Now</span>
+            </p>
+          ) : (
+            <p>
+              Already have account?{" "}
+              <span onClick={() => setSignState("Sign In")}>Sign In Now</span>
+            </p>
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
